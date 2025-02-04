efe2804cd4f9d4d8209f426bd804702e948c8232
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-02-04 09:30:06 +0530

    Create footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div className='bg-[#acbbff] h-[7vh] sticky bottom-0 flex w-full justify-around items-center text-blue-900'>
+        "This website is fully functional and designed to provide a complete experience. However, due to Google’s restrictions, we have temporarily disabled user data collection. You can explore all features without signing in or providing personal information."
+    </div>
+  )
+}
+
+export default footer
