efeaa56c015a5c636371ae72e17f6a21dca53686
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-02-04 09:40:20 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const footer = () => {
   return (
     <div className='bg-[#acbbff] h-[7vh] sticky bottom-0 flex w-full justify-around items-center text-blue-900'>
-        "This website is fully functional and designed to provide a complete experience. However, due to Google’s restrictions, we have temporarily disabled user data collection. You can explore all features without signing in or providing personal information."
-    </div>
+      
+"This website is fully functional, but user data collection is disabled due to Google’s restrictions. Enjoy exploring!"    </div>
   )
 }
 
