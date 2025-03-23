65f7c9a51495fbe42eb69547c67d36b8bc4ea6dc
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-03-23 22:09:44 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className='bg-[#acbbff] h-[7vh] flex w-full justify-around items-center text-blue-900'>
+        <div className='font-bold text-[26px] cursor-pointer hover:underline underline-offset-8'>iTask</div>
+        <ul className='flex gap-5 items-center'>
+            <li className='cursor-pointer font-semibold text-[16px] hover:underline underline-offset-4'>Home</li>
+            <li className='cursor-pointer font-semibold text-[16px] hover:underline underline-offset-4'>Your Tasks</li>
+        </ul>
+    </div>
+  )
+}
+
+export default navbar
